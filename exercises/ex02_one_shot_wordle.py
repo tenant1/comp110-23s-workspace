74cfd6e98a75c06f4bdba42ff508b1701e43b4ba
Kenan Tart <tenant@ad.unc.edu>
2023-02-06 17:38:57 -0500

    one shot wordle #2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -12,12 +12,11 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-Continue = True
-while (Continue) : 
-
+continued = True
+while(continued): 
     if len(term) != word_length:
-        term = input (f"That was not {word_length} letters! Try again: " )
-    else : 
+        term = input(f"That was not {word_length} letters! Try again: ")
+    else: 
         #establish a variable to keep track of the index of the word you are checking and initialize it to the first index of a string
         index = 0
 
@@ -25,36 +24,36 @@ while (Continue) :
         guess = ""
 
         #While the index variable is less than the length of the secret word
-        while (index < word_length) :
+        while (index < word_length):
             #Test to see if the current index of the guessed word is equal to the same index of your secret word
-            if (word[index] == term[index]) : 
+            if (word[index] == term[index]): 
                 # concatenate a GREEN_BOX emoji to the result string variable you setup previously
                 guess = guess + GREEN_BOX 
-            else :
+            else:
                 #Declare a boolean variable to keep track of whether the guessed character exists anywhere else in the word and initialize it to False.
                 character_guessed = False
                 #Declare a variable to keep track of the alternate indices of the secret you are checking for a match and initialize it to 0. 
                 alternate_index = 0
                 #Write while loop with the following test logic: while the boolean variable is not true and the index is less than the length of the secret
-                while ((character_guessed == False) and (alternate_index < word_length)):
+                while((character_guessed is False) and (alternate_index < word_length)):
                     #Test to see if the alternate index of the secret word is equal to the current index of the guessed word.
-                    if (word[alternate_index] == term[index]) : 
+                    if (word[alternate_index] == term[index]): 
                         character_guessed = True 
                     #Otherwise, increment your alternate index variable.
-                    else :
+                    else:
                         alternate_index += 1
                 
-                if (character_guessed == True) :
+                if (character_guessed is True):
                     guess = guess + YELLOW_BOX 
-                else : 
-                    #concatenate a WHITE_BOX emoji.
+                else: 
+                    #concatenate a WHITE_BOX emoji
                     guess = guess + WHITE_BOX
             index += 1
         
         print(guess)
-        if (word == term) :
+        if (word == term):
             print("Woo! You got it!")
-        else :
+        else:
             print("Not quite. Play again soon!")
         
-        Continue = False
+        continued = False
