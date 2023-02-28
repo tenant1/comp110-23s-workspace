21f4cf10eeed4d21d4665f42b3b6dc5d1b3612b8
Kenan Tart <tenant@ad.unc.edu>
2023-02-28 18:36:53 -0500

    EX04 Attempt 2

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,23 +1,18 @@
 """EX04 - Utils."""
 __author__ = "730489449"
 
-#Given a list of ints and an int, all should return a bool 
-# indicating whether or not all the ints in the list are the same as
-#  the given int. 
 def all(nums: list[int], k: int) -> bool:
     for num in nums: 
         if num != k: 
             return False
     return True  
 
-#The max function is given a list of ints, max should return the 
-# largest in the List.
 def max(nums: list[int]) -> int:
-    #If the list is empty, max should result in a ValueError. 
     if len(nums) == 0:
         raise ValueError ("max() arg is an empty list")
     
     result: int = nums[0]
+
 
     for num in nums: 
         if num > result: 
@@ -25,8 +20,6 @@ def max(nums: list[int]) -> int:
 
     return result
 
-#Given two lists of int values, return True if every element at 
-# every index is equal in both lists.
 def is_equal(nums_1: list[int], nums_2: list[int]) -> bool:
 
     if len(nums_1) != len(nums_2): 
@@ -38,6 +31,3 @@ def is_equal(nums_1: list[int], nums_2: list[int]) -> bool:
         
     return True
     
-
-print (all([1, 2, 3, 4,], 2)) 
-print (max)
