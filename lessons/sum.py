b7c9141e1d87e5b60dc6ca29a0f0eb187587aca9
Kenan Tart <tenant@ad.unc.edu>
2023-03-05 16:14:04 -0500

    sum.py lessons commit #2

--- lessons/sum.py
+++ lessons/sum.py
@@ -4,7 +4,7 @@ def sun(xs: list[float]) -> float:
     """return sum of all elements in xs"""
     sum_total: float  = 0.0
     idx: int = 0
-    if idx < len(xs):
+    for idx in len(xs):
         sum_total += xs[idx]
         idx += 1
-    return sum
+    print(sum)
