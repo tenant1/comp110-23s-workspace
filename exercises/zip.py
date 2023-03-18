0ac3c8503ca4187f093212edd49cd5fe0538ca61
Kenan Tart <tenant@ad.unc.edu>
2023-03-18 15:13:28 -0400

    CQ04 Attempt 1

---
+++ exercises/zip.py
@@ -0,0 +1,12 @@
+"""CQ04 - Dict Function Writing"""
+
+__author__ = "730489449"
+
+def zip(list_1: list[str], list_2: list[int]) -> dict[str,int]: 
+    if len(list_1) == len(list_2) and len(list_1)!=0 and len(list_2)!=0: 
+         zip = {list_1[i]: list_2[i]}
+         for i in range: len(list_1):
+         else: 
+            if len(list_1)!=len(list_2): 
+                return dict()
+    return zip 
