2dffbffe3d87e679a5ec6fcca725c22fd12de204
Kenan Tart <tenant@ad.unc.edu>
2023-03-20 11:06:06 -0400

    cq04 attempt 2

--- exercises/zip.py
+++ exercises/zip.py
@@ -3,10 +3,10 @@
 __author__ = "730489449"
 
 def zip(list_1: list[str], list_2: list[int]) -> dict[str,int]: 
+    zip = {}
     if len(list_1) == len(list_2) and len(list_1)!=0 and len(list_2)!=0: 
-         zip = {list_1[i]: list_2[i]}
-         for i in range: len(list_1):
-         else: 
-            if len(list_1)!=len(list_2): 
-                return dict()
+         for i in range(0, len(list_1)):
+             zip[list_1[i]]=list_2[i] 
+    if len(list_1)!=len(list_2): 
+        return dict()
     return zip 
