d8da317c3b5c4c0c4ea76afa9e9aa3f80ea81011
Kenan Tart <tenant@ad.unc.edu>
2023-03-20 13:35:56 -0400

    ex04 attempt 3

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -7,6 +7,10 @@ points: int
 player: str
 heart_eyes: str
 def main(): 
+    """Define a main function, that returns None, and follows the 
+    Python idiom for calling main at the end of the program based 
+    on the dunderscore variable __name__ being equal to "__main__".
+    """
     global points, player, heart_eyes 
     points = 0
     player = ""
@@ -26,33 +30,40 @@ def main():
             print("Invalid choice, try again!")
 
 def greet() -> None:
-    """The greet function must print a welcome message to give some context to your 
-    game and asks the player for their name. """
+    """
+    The greet function must print a welcome message to give some context to your 
+    game and asks the player for their name. 
+    """
+
     global player 
     print(f"Hello! {heart_eyes}")
     player = input("What is your name?")
 
 def procedure_1() -> None:
-    """The procedure should lead to textual interaction(s) that make use of players 
-    name and ask the player for additional input."""
+    """
+    The procedure should lead to textual interaction(s) that make use of players 
+    name and ask the player for additional input.
+    """
     global player, points
     character = input(f"What Harry Potter character are you, {player}?")
     multiplier = random.randint(1,3)
     if character == "Harry": 
-        points += 3*multiplier
+        points+=3*multiplier
     elif character == "Hermione":
-        points += 5*multiplier
+        points+=5*multiplier
     elif character == "Snape":
-        points += 2*multiplier
+        points+=2*multiplier
     elif character == "Draco":
-        points += 4*multiplier
+        points+=4*multiplier
     else: 
-        points += 1*multiplier
+        points+=1*multiplier
 
 def procedure_2(number: int) -> int: 
-    """The function takes at least one int as a parameter and returns an int. The call 
+    """
+    The function takes at least one int as a parameter and returns an int. The call 
     to this function from main should pass the player’s points value as an argument to 
-    the function call"""
+    the function call
+    """
     global player 
     character = input(f"Pick a number 1-5, {player}.")
     if character == "1":
@@ -69,5 +80,5 @@ def procedure_2(number: int) -> int:
         return number 
 
 if __name__ == "__main__": 
-    main()
 
+    main()
