f5e4752011632f38118cc9e09a2bcf5de94bb7c1
Kenan Tart <tenant@ad.unc.edu>
2023-03-21 12:24:24 -0400

    cyoa attempt 3

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -6,8 +6,11 @@ import random
 points: int = 0
 player: str = ""
 heart_eyes: str = "\U0001F60D"
+
+
 def main(): 
-    """Define a main function, that returns None, and follows the 
+    """
+    Define a main function, that returns None, and follows the 
     Python idiom for calling main at the end of the program based 
     on the dunderscore variable __name__ being equal to "__main__".
     """
@@ -26,6 +29,7 @@ def main():
         else: 
             print("Invalid choice, try again!")
 
+
 def greet() -> None:
     """
     The greet function must print a welcome message to give some context to your 
@@ -36,6 +40,7 @@ def greet() -> None:
     print(f"Hello! {heart_eyes}")
     player = input("What is your name?")
 
+
 def procedure_1() -> None:
     """
     The procedure should lead to textual interaction(s) that make use of players 
@@ -45,15 +50,16 @@ def procedure_1() -> None:
     character = input(f"What Harry Potter character are you, {player}?")
     multiplier = random.randint(1,3)
     if character == "Harry": 
-        points+=3*multiplier
+        points += 3 * multiplier
     elif character == "Hermione":
-        points+=5*multiplier
+        points += 5 * multiplier
     elif character == "Snape":
-        points+=2*multiplier
+        points += 2 * multiplier
     elif character == "Draco":
-        points+=4*multiplier
+        points += 4 * multiplier
     else: 
-        points+=1*multiplier
+        points += 1 * multiplier
+
 
 def procedure_2(number: int) -> int: 
     """
@@ -75,6 +81,7 @@ def procedure_2(number: int) -> int:
         return number + 5
     else: 
         return number 
+
 
 if __name__ == "__main__": 
 
